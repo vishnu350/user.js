9c0b161ff21029f4e1f536925f26c53af3f37658
Vishnu <5123344+vishnu350@users.noreply.github.com>
2023-12-15 20:04:22 +0800

    Disabled private mode search engine.

--- user.js
+++ user.js
@@ -1192,6 +1192,7 @@ user_pref("_user.js.parrot", "SUCCESS: No no he's not dead, he's, he's restin'!"
 
 /* vishnup custom changes */
 // Misc changes
+user_pref("browser.search.separatePrivateDefault.ui.enabled", false); // Disable private search
 user_pref("browser.startup.page", 3); // Enable session restore
 user_pref("keyword.enabled", true); // This causes the urlbar to not use the search engine
 user_pref("signon.rememberSignons", false); // Dont save passwords
